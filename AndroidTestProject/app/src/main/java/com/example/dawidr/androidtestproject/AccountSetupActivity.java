54b015f7e3787a0c9d09f3b877c6619498f69aea
dawidrichert@gmail.com <DimaKaru2@>
2014-07-28 12:56:43 +0200

    Added Account Setup screen. Account details are stored in SharedPreferences.

---
+++ AndroidTestProject/app/src/main/java/com/example/dawidr/androidtestproject/AccountSetupActivity.java
@@ -0,0 +1,68 @@
+package com.example.dawidr.androidtestproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AccountSetupActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_setup);
+
+        EditText etUserName = (EditText) findViewById(R.id.etUserName);
+        EditText etEmail = (EditText) findViewById(R.id.etEmail);
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                Button btnContinue = (Button) findViewById(R.id.btnContinue);
+                String strUserName = ((EditText) findViewById(R.id.etUserName)).getText().toString();
+                String strEmail = ((EditText) findViewById(R.id.etEmail)).getText().toString();
+
+                if (strUserName.length() != 0 && strEmail.length() != 0)
+                    btnContinue.setEnabled(true);
+                else
+                    btnContinue.setEnabled(false);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        };
+
+        etUserName.addTextChangedListener(textWatcher);
+        etEmail.addTextChangedListener(textWatcher);
+    }
+
+    public void ContinueClick(View view) {
+
+        Button btnContinue = (Button) findViewById(R.id.btnContinue);
+        String strUserName = ((EditText) findViewById(R.id.etUserName)).getText().toString();
+        String strEmail = ((EditText) findViewById(R.id.etEmail)).getText().toString();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.dawidr.androidtestproject", MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("user_name", strUserName);
+        editor.putString("email", strEmail);
+        editor.commit();
+
+        Intent intent = new Intent(AccountSetupActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
