21fbdfdd34143984f4e44eddfdfcf98bfc1875c7
dawidrichert@gmail.com <DimaKaru2@>
2014-07-28 14:45:28 +0200

    Added unit tests.

---
+++ AndroidTestProject/app/src/androidTest/java/com/example/dawidr/androidtestproject/AccountSetupTest.java
@@ -0,0 +1,51 @@
+package com.example.dawidr.androidtestproject;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AccountSetupTest extends ActivityInstrumentationTestCase2<AccountSetupActivity> {
+
+    private AccountSetupActivity accountSetupActivity;
+    private EditText etUserName;
+    private EditText etEmail;
+    private Button btnContinue;
+
+    public AccountSetupTest() {
+        super(AccountSetupActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        accountSetupActivity = getActivity();
+
+        etUserName = (EditText) accountSetupActivity.findViewById(R.id.etUserName);
+        etEmail = (EditText) accountSetupActivity.findViewById(R.id.etEmail);
+        btnContinue = (Button) accountSetupActivity.findViewById(R.id.btnContinue);
+    }
+
+    public void test_validate() {
+        accountSetupActivity.runOnUiThread(new Runnable() {
+            public void run() {
+
+                etUserName.setText("");
+                etEmail.setText("");
+                assertEquals(btnContinue.isEnabled(), false);
+
+                etUserName.setText("something");
+                etEmail.setText("");
+                assertEquals(btnContinue.isEnabled(), false);
+
+                etUserName.setText("");
+                etEmail.setText("something");
+                assertEquals(btnContinue.isEnabled(), false);
+
+                etUserName.setText("something");
+                etEmail.setText("something");
+                assertEquals(btnContinue.isEnabled(), true);
+            }
+        });
+    }
+}
--- AndroidTestProject/app/src/androidTest/java/com/example/dawidr/androidtestproject/ApplicationTest.java
+++ AndroidTestProject/app/src/androidTest/java/com/example/dawidr/androidtestproject/ApplicationTest.java
@@ -3,11 +3,20 @@ package com.example.dawidr.androidtestproject;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    public void test_first() throws Exception {
+        final int variable1 = 5;
+        final int variable2 = 5;
+        assertEquals(variable1, variable2);
+    }
+
+//    public void test_second() throws Exception {
+//        final int variable1 = 5;
+//        final int variable2 = 2;
+//        assertEquals(variable1, variable2);
+//    }
 }
